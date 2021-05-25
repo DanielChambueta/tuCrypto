4e8729e09d25eed80344eb63b33d944d3cd3b617
DanielChambueta <chambuetadaniel@gmail.com>
2021-05-24 19:55:45 -0500

    Lista con FlatList

--- src/components/Coins/CoinsScreen.js
+++ src/components/Coins/CoinsScreen.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { View, FlatList, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import Http from 'tuCrypto/src/libs/http';
+import CoinsItem from './CoinsItem';
 
 class CoinsScreen extends Component {
 
+  state = {
+    coins:[],
+    loading: false
+  }
+
   componentDidMount = async () => {
-    const coins = Http.instance.get("https://api.coinlore.net/api/tickers/");
-    console.log("coins",coins);
+    this.setState({ loading:true });
+    const res = await Http.instance.get("https://api.coinlore.net/api/tickers/");
+    this.setState({ coins:res.data, loading: false });
   }
 
   handlePress = () => {
@@ -15,12 +22,23 @@ class CoinsScreen extends Component {
   }
 
   render() {
+    const { coins, loading } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.titleText}>Coins Screen</Text>
-        <Pressable style={styles.btn} onPress={this.handlePress}>
-          <Text style={styles.btnText}>Ir a detail</Text>
-        </Pressable>
+        {loading ?
+          <ActivityIndicator 
+          style={styles.loader}
+          color="#FFF" 
+          size="large"             
+          />
+          : null 
+        }
+        <FlatList
+          data={coins}
+          renderItem={({ item }) => 
+          <CoinsItem item={item} />
+          }
+        />
       </View>
     );
   }
@@ -29,7 +47,7 @@ class CoinsScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: "#FFF",
   },
   titleText: {
     color: "#FFF",
@@ -44,6 +62,9 @@ const styles = StyleSheet.create({
   btnText: {
     color: "#fff",
     textAlign: "center"
+  },
+  loader: {
+    marginTop: 60
   }
 
 });
